d0c2c9ec605b727ef346cf81164f0f37950becfa
kearneyg20428 <kearneyg20428@student.cghsnc.org>
2017-09-21 21:16:03 -0400

    Added a hardware class
    I added a simple hardware class that can be used with mechanum wheels

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/HardwareDxm.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+/**
+ * Created by kearneyg20428 on 9/21/2017.
+ */
+
+public class HardwareDxm {
+/*
+I created this naming system as the letter f or b meaning front or back because
+with machanum wheels all 4 wheels need to be motorized
+*/
+
+    public DcMotor fLeft = null;
+    public DcMotor fRight = null;
+    public DcMotor bLeft = null;
+    public DcMotor bRight = null;
+
+    HardwareMap hwMap = null;
+    private ElapsedTime period = new ElapsedTime();
+
+    public HardwareDxm(HardwareMap ahwMap)
+    {
+    hwMap = ahwMap;
+
+        fLeft = hwMap.dcMotor.get("fLeft");
+        fRight = hwMap.dcMotor.get("fRight");
+        bLeft = hwMap.dcMotor.get("bLeft");
+        bRight = hwMap.dcMotor.get("bRight");
+
+        fLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        fRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        bLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        bRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        fLeft.setPower(0);
+        fRight.setPower(0);
+        bLeft.setPower(0);
+        bRight.setPower(0);
+
+        fLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void init()
+    {
+
+    }
+}
