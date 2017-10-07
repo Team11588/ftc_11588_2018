52d89fd846f1793c27eeb99ade74ccf171bd3406
kearneyg20428 <kearneyg20428@student.cghsnc.org>
2017-10-07 12:56:46 -0400

    Claw testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/HardwareDxm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/HardwareDxm.java
@@ -23,10 +23,7 @@ with machanum wheels all 4 wheels need to be motorized
     public DcMotor fRight = null;
     public DcMotor bLeft = null;
     public DcMotor bRight = null;
-    public DcMotor lateralClaw = null;
-    public DcMotor verticalClaw = null;
 
-    public Servo claw = null;
 
     HardwareMap hwMap = null;
     private ElapsedTime period = new ElapsedTime();
@@ -39,33 +36,25 @@ with machanum wheels all 4 wheels need to be motorized
         fRight = hwMap.dcMotor.get("fRight");
         bLeft = hwMap.dcMotor.get("bLeft");
         bRight = hwMap.dcMotor.get("bRight");
-        lateralClaw = hwMap.dcMotor.get("lateralClaw");
-        verticalClaw = hwMap.dcMotor.get("verticalClaw");
 
-        //claw = hwMap.servo.get("cl    aw");
 
         fLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         fRight.setDirection(DcMotorSimple.Direction.FORWARD);
         bLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         bRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        lateralClaw.setDirection(DcMotorSimple.Direction.FORWARD);
-        verticalClaw.setDirection(DcMotorSimple.Direction.FORWARD);
+
 
         fLeft.setPower(0);
         fRight.setPower(0);
         bLeft.setPower(0);
         bRight.setPower(0);
-        lateralClaw.setPower(0);
-        verticalClaw.setPower(0);
 
-        claw.setPosition(0);
+
 
         fLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         fRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         bLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         bRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        lateralClaw.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        verticalClaw.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Theoretical_Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Theoretical_Claw.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.teamcode.Hardware.HardwareDxm;
+import org.firstinspires.ftc.teamcode.Hardware.TheoreticalHardwareDxm;
 
 /**
  * Created by kearneyg20428 on 10/3/2017.
@@ -16,14 +16,9 @@ import org.firstinspires.ftc.teamcode.Hardware.HardwareDxm;
 @TeleOp(name = "Claw" , group = "TeleOp")
 public class Theoretical_Claw extends OpMode {
 
-    HardwareDxm robot = new HardwareDxm();
+    TheoreticalHardwareDxm robot = new TheoreticalHardwareDxm();
 
-    double leftY2 = -gamepad2.left_stick_y;
-    double rightY2 = -gamepad2.right_stick_y;
-    double triggerR2 = gamepad2.right_trigger;
-    double triggerL2 = gamepad2.left_trigger;
-
-    final static double SERVO_SHIFT = 0.075;
+    final static double SERVO_SHIFT = 0.05;
 
 
     public void init() {
@@ -32,16 +27,18 @@ public class Theoretical_Claw extends OpMode {
 
     public void loop() {
 
-        robot.verticalClaw.setPower(leftY2);
-        robot.lateralClaw.setPower(rightY2);
+        double leftY2 = -gamepad2.left_stick_y;
+        double rightY2 = -gamepad2.right_stick_y;
+        double triggerR2 = gamepad2.right_trigger;
+        double triggerL2 = gamepad2.left_trigger;
 
-       if (triggerR2 > 0.5)
+        if (triggerL2 > 0.5)
        {
            double position = robot.claw.getPosition();
            double newPosition = Range.clip( position + SERVO_SHIFT, Servo.MIN_POSITION, Servo.MAX_POSITION);
            robot.claw.setPosition(newPosition);
        }
-       else if (triggerL2 > .5)
+       else if (triggerR2 > .5)
        {
            double position = robot.claw.getPosition();
            double newPosition = Range.clip( position - SERVO_SHIFT, Servo.MIN_POSITION, Servo.MAX_POSITION);
