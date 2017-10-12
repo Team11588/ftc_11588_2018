f66a73df93f436465928fdfc556edf60c665de64
kearneyg20428 <kearneyg20428@student.cghsnc.org>
2017-10-12 17:28:07 -0400

    IMU Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/IMUTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/IMUTest.java
@@ -1,13 +1,136 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import java.util.Locale;
+
+
+import org.firstinspires.ftc.teamcode.Hardware.TheoriticalTank;
 /**
  * Created by Kearneyg20428 on 10/10/2017.
  */
 
 @Autonomous (name = "IMU Test")
-public class IMUTest extends SensorBNO055IMU{
+public class IMUTest extends OpMode {
+
+    TheoriticalTank robot = new TheoriticalTank();
+    HardwareMap hwMap           =  null;
+
+    BNO055IMU imu;
+    Orientation angles;
+    Acceleration gravity;
+
+    final double TURN_POWER = .75;
+
+
+
+    @Override
+    public void init() {
+
+        robot.init(hardwareMap);
+
+        // The IMU sensor object
+
+
+        // State used for updating telemetry
+        Orientation angles;
+        Acceleration gravity;
+
+        //----------------------------------------------------------------------------------------------
+        // Main logic
+        //----------------------------------------------------------------------------------------------
+
+
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        // Set up our telemetry dashboard
+
+
+    }
+
+    @Override
+    public void loop() {
+
+        telemetry.addData("I made it","");
+        telemetry.update();
+
+        if (angles != null) {
+
+            if (angles.firstAngle < 90) {
+                robot.leftMotor.setPower(-TURN_POWER);
+                robot.rightMotor.setPower(TURN_POWER);
+            } else {
+                robot.leftMotor.setPower(0);
+                robot.rightMotor.setPower(0);
+            }
+        }
+
+
+        // At the beginning of each telemetry update, grab a bunch of data
+        // from the IMU that we will then display in separate lines.
+        telemetry.addAction(new Runnable() {
+            @Override
+            public void run() {
+                // Acquiring the angles is relatively expensive; we don't want
+                // to do that in each of the three items that need that info, as that's
+                // three times the necessary expense.
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                gravity = imu.getGravity();
+            }
+        });
+
+        telemetry.addData("heading", new Func<String>() {
+                    @Override
+                    public String value() {
+                        return formatAngle(angles.angleUnit, angles.firstAngle);
+                    }
+                });
+    }
+
+    String formatAngle (AngleUnit angleUnit,double angle){
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
