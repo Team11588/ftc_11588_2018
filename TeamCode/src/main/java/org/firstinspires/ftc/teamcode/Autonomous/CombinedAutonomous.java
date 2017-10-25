6d6abeacc465f10b3b8edccca426d046369a580b
kearneyg20428 <kearneyg20428@student.cghsnc.org>
2017-10-25 19:04:33 -0400

    I made the vuforia work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CombinedAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CombinedAutonomous.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import android.support.annotation.Nullable;
 import android.text.ParcelableSpan;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
@@ -17,6 +18,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -28,6 +30,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.firstinspires.ftc.teamcode.Hardware.HardwareDxm;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 import java.util.Locale;
 
@@ -42,57 +46,51 @@ import java.util.Locale;
 &&&&&&&&&&&&&&&&&&&&&&&&Picture Taking&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 ????????????????????????Pixel Testing??????????????????????????????????
 ~~~~~~~~~~~~~~~~~~~~~~~~Mechanum Driving~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
  */
+
+@Autonomous (name = "Combined Autonomous")
 public class CombinedAutonomous extends LinearOpMode{
+
     HardwareDxm robot = new HardwareDxm();
     HardwareMap hwMap = null;
-//**************************************************************************************************
+    //**************************************************************************************************
     VuforiaLocalizer vuforia;
-//**************************************************************************************************
+    //**************************************************************************************************
 
-//??????????????????????????????????????????????????????????????????????????????????????????????????
+    //??????????????????????????????????????????????????????????????????????????????????????????????????
     String filePath = "Pictures";
     String imageName = "TestImage.JPG";
     private ElapsedTime runtime = new ElapsedTime();
-//??????????????????????????????????????????????????????????????????????????????????????????????????
+    //??????????????????????????????????????????????????????????????????????????????????????????????????
     @Override
-    public void runOpMode()  {
+    public void runOpMode() {
 
-        initProcess();
+        robot.init(hardwareMap);
 
-        waitForStart();
+        //----------------------------------------------------------------------------------------------
+       BNO055IMU imu;
 
+        // State used for updating telemetry
+        Orientation angles;
+        Acceleration gravity;
 
-    }
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
 
-public void initProcess(){
+        //----------------------------------------------------------------------------------------------
 
-    robot.init(hardwareMap);
-    //----------------------------------------------------------------------------------------------
-    BNO055IMU imu;
-
-    // State used for updating telemetry
-    Orientation angles;
-    Acceleration gravity;
-
-    BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-    parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-    parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-    parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
-    parameters.loggingEnabled = true;
-    parameters.loggingTag = "IMU";
-    parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-
-    imu = hardwareMap.get(BNO055IMU.class, "imu");
-    imu.initialize(parameters);
-    //----------------------------------------------------------------------------------------------
-
-    //**********************************************************************************************
+        //**********************************************************************************************
 
 
-    OpenGLMatrix lastLocation = null;
+        OpenGLMatrix lastLocation = null;
 
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
@@ -110,7 +108,41 @@ public void initProcess(){
         VuforiaTrackable relicTemplate = relicTrackables.get(0);
         relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
 
+        waitForStart();
+
+        relicTrackables.activate();
+
+
+        int mark = 0;
+        /*
+        Right - 1
+        Center - 2
+        Left - 3
+         */
+
+        do {
+
+            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+            if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
+
+                if (vuMark == RelicRecoveryVuMark.RIGHT) {
+                    mark = 1;
+                    telemetry.addData("RIGHT", "%s", vuMark);
+                }
+                else if (vuMark == RelicRecoveryVuMark.CENTER) {
+                    telemetry.addData("CENTER", "");
+                    mark = 2;
+                }
+                else if (vuMark == RelicRecoveryVuMark.LEFT) {
+                    telemetry.addData("Left", "");
+                    mark = 3;
+                }
+
+                telemetry.update();
+            }
+        }while(mark == 0);
+    }
         //**********************************************************************************************
-}
+
 }
 
