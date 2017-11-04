2efe1a2064ab18504d015bb3503c8efb286f81d3
piscullin18641 <32109807+piscullin18641@users.noreply.github.com>
2017-11-04 12:29:35 -0400

    push origin

--- FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/JewelView.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/JewelView.java
@@ -25,7 +25,7 @@ public class JewelView extends View {
         Paint p = new Paint();
         //scan box
         p.setColor(Color.argb(100,0,0,0));
-        canvas.drawRect((int) (20*xPercent),(int) (60*yPercent), (int) (30*xPercent),(int) (80*yPercent), p);
+        canvas.drawRect((int) (30*xPercent),(int) (30*yPercent), (int) (52.5*xPercent),(int) (45*yPercent), p);
 
         //top
         p.setColor(Color.argb(200,255,0,0));
