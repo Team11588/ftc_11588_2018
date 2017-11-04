7715ac4edc38a663a0c9218ba538acd2a8c708a9
StephenButters <32017706+Stephen013@users.noreply.github.com>
2017-11-04 12:25:13 -0400

    Created isOurJewelonLeft

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CombinedAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CombinedAutonomous.java
@@ -5,48 +5,30 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.os.Environment;
-import android.support.annotation.Nullable;
-import android.text.ParcelableSpan;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcontroller.internal.LinearOpModeCamera;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.Func;
-import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
-import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
+
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.firstinspires.ftc.teamcode.Hardware.HardwareDxm;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Locale;
 
 /**
  * Created by kearneyg20428 on 10/23/2017.
@@ -61,13 +43,13 @@ import java.util.Locale;
 ~~~~~~~~~~~~~~~~~~~~~~~~Mechanum Driving~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
 
-@Autonomous (name = "Combined Autonomous")
-public class CombinedAutonomous extends LinearOpModeCamera{
-public static final int SAMPLE_LEFT_X_PCT=20;
-public static final int SAMPLE_RIGHT_X_PCT=30;
-public static final int SAMPLE_TOP_Y_PCT=60;
-public static final int SAMPLE_BOT_Y_PCT=80;
-
+@Autonomous(name = "Combined Autonomous")
+public class CombinedAutonomous extends LinearOpModeCamera {
+    public static final int SAMPLE_LEFT_X_PCT = 20;
+    public static final int SAMPLE_RIGHT_X_PCT = 30;
+    public static final int SAMPLE_TOP_Y_PCT = 60;
+    public static final int SAMPLE_BOT_Y_PCT = 80;
+    public static final String TEAM_COLOR = "red";
     HardwareDxm robot = new HardwareDxm();
     HardwareMap hwMap = null;
 
@@ -89,11 +71,10 @@ public static final int SAMPLE_BOT_Y_PCT=80;
     public void runOpMode() throws InterruptedException {
 
 
-
         robot.init(hardwareMap);
 
         //----------------------------------------------------------------------------------------------
-       BNO055IMU imu;
+        BNO055IMU imu;
 
         Orientation angles;
         Acceleration gravity;
@@ -114,15 +95,13 @@ public static final int SAMPLE_BOT_Y_PCT=80;
         //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
 
-
         //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-        telemetry.addData("ready" , "");
+        telemetry.addData("ready", "");
         telemetry.update();
 
 
         waitForStart();
 // The init process has finished by here
-
 
 
         //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
@@ -133,14 +112,13 @@ public static final int SAMPLE_BOT_Y_PCT=80;
 
         if (isCameraAvailable()) {
             startCamera();
-            while(yuvImage == null);
+            while (yuvImage == null) ;
             Bitmap rgbImage = convertYuvImageToRgb(yuvImage, width, height, 0);
             stopCamera();
-            saveBitmap(imageName,rgbImage);
+            saveBitmap(imageName, rgbImage);
         }
 
         //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
 
 
         //**********************************************************************************************
@@ -149,68 +127,34 @@ public static final int SAMPLE_BOT_Y_PCT=80;
 
         File sd = Environment.getExternalStorageDirectory();
 
-        if (sd == null){
+        if (sd == null) {
             telemetry.addLine("Open External Storage Failed");
         }
 
-        File image = new File(sd+"/"+filePath, imageName);
+        File image = new File(sd + "/" + filePath, imageName);
 
         if (image == null) {
             telemetry.addLine("Open Image File Failed");
-        }
-        else {
+        } else {
             telemetry.addLine("Open Image Successful");
         }
-        telemetry.addData("Image Name", "%s",image.getAbsolutePath());
-
+        telemetry.addData("Image Name", "%s", image.getAbsolutePath());
 
 
         BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        Bitmap bitmap = BitmapFactory.decodeFile(image.getAbsolutePath(),bmOptions);
+        Bitmap bitmap = BitmapFactory.decodeFile(image.getAbsolutePath(), bmOptions);
 
         if (bitmap == null) {
             telemetry.addLine("Could not read bitmap");
 
         }
-        int LeftRed = 0;
-        int LeftBlue = 0;
-        int count = 0;
 
-        double xPercent = (bitmap.getWidth())/100.0;
-        double yPercent = (bitmap.getHeight())/100.0;
-
-        telemetry.addData("Start For loop", "");
-        for(int x=SAMPLE_LEFT_X_PCT; x<SAMPLE_RIGHT_X_PCT; x++){ // replace 200 with x pixel size value
-            for(int y=SAMPLE_TOP_Y_PCT;y<SAMPLE_BOT_Y_PCT;y++){
-                int color = bitmap.getPixel((int) (x*xPercent),(int) (y*yPercent));
-                //telemetry.addData("Color", "%d", color);
-                count++;
-                int red = Color.red(color);
-                int green = Color.green(color);
-                int blue = Color.blue(color);
-
-                double[] HBV = RGBtoHSV(red, green, blue);
-                double hue = HBV[0];
-
-                if(((300<hue)||(hue<60)))
-                    LeftRed = LeftRed + 1;
-
-                if(((180<hue)&&(hue<=300)))
-                    LeftBlue = LeftBlue + 1;
-            }
-        }
         drawSamplingBox(bitmap);
 
-
-        telemetry.addData("Red count", "%d", LeftRed);
-        telemetry.addData("Blue count", "%d", LeftBlue);
-        telemetry.addData("Count", "%d", count);
-        telemetry.update();
-
-        if (LeftRed > LeftBlue){
+        if (isOurJewelonLeft(bitmap)) {
             robot.jewelKnockDevice.setPosition(0);
-            drive(.5,.5,.5,.5);
-            drive(0,0,0,0);
+            drive(.5, .5, .5, .5);//,drive forward
+            drive(0, 0, 0, 0);//stop
         }
         //??????????????????????????????????????????????????????????????????????????????????????????????????
 
@@ -253,13 +197,11 @@ public static final int SAMPLE_BOT_Y_PCT=80;
 
                 if (vuMark == RelicRecoveryVuMark.RIGHT) {
                     mark = 1;
-                    telemetry.addData("RIGHT","");
-                }
-                else if (vuMark == RelicRecoveryVuMark.CENTER) {
+                    telemetry.addData("RIGHT", "");
+                } else if (vuMark == RelicRecoveryVuMark.CENTER) {
                     telemetry.addData("CENTER", "");
                     mark = 2;
-                }
-                else if (vuMark == RelicRecoveryVuMark.LEFT) {
+                } else if (vuMark == RelicRecoveryVuMark.LEFT) {
                     telemetry.addData("Left", "");
                     mark = 3;
                 }
@@ -267,27 +209,27 @@ public static final int SAMPLE_BOT_Y_PCT=80;
                 telemetry.update();
             }
         }
-        while(mark == 0);
+        while (mark == 0);
 
         //**********************************************************************************************
 
-        while(opModeIsActive());    
+        while (opModeIsActive()) ;
     }
 
 
-        public static double[] RGBtoHSV(double r, double g, double b){
+    public static double[] RGBtoHSV(double r, double g, double b) {
 
-            double h, s, v;
+        double h, s, v;
 
-            double min, max, delta;
+        double min, max, delta;
 
-            min = Math.min(Math.min(r, g), b);
-            max = Math.max(Math.max(r, g), b);
+        min = Math.min(Math.min(r, g), b);
+        max = Math.max(Math.max(r, g), b);
 
-            // V
-            v = max;
+        // V
+        v = max;
 
-            delta = max - min;
+        delta = max - min;
 
 
         /*
@@ -300,60 +242,105 @@ public static final int SAMPLE_BOT_Y_PCT=80;
             return new double[]{h,s,v};
         }*/
 
-            // H
-            if( r == max )
-                h = ( g - b ) / delta; // between yellow & magenta - reds
-            else if( g == max )
-                h = 2 + ( b - r ) / delta; // between cyan & yellow - greens
-            else
-                h = 4 + ( r - g ) / delta; // between magenta & cyan - blues
+        // H
+        if (r == max)
+            h = (g - b) / delta; // between yellow & magenta - reds
+        else if (g == max)
+            h = 2 + (b - r) / delta; // between cyan & yellow - greens
+        else
+            h = 4 + (r - g) / delta; // between magenta & cyan - blues
 
-            h *= 60;    // degrees
+        h *= 60;    // degrees
 
-            if( h < 0 )
-                h += 360;
+        if (h < 0)
+            h += 360;
 
-            return new double[]{h,/*s,*/v};
-        }
+        return new double[]{h,/*s,*/v};
+    }
 
-        public static void saveBitmap(String filename, Bitmap bitmap) {
+    public static void saveBitmap(String filename, Bitmap bitmap) {
 
-            String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/Pictures";
-            FileOutputStream fileOutputStream = null;
+        String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pictures";
+        FileOutputStream fileOutputStream = null;
 
-            try {
-                fileOutputStream = new FileOutputStream(filePath + "/" + filename);
-                bitmap.compress( Bitmap.CompressFormat.PNG, 100, fileOutputStream);
-                fileOutputStream.flush();
-                fileOutputStream.close();
-            }
-
-            catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
+        try {
+            fileOutputStream = new FileOutputStream(filePath + "/" + filename);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
 
     }
 
-        public  void drive (double fL,double fR, double bL, double bR){
-            robot.fLeft.setPower(fL);
-            robot.fRight.setPower(fR);
-            robot.bLeft.setPower(bL);
-            robot.bRight.setPower(bR);
+    public void drive(double fL, double fR, double bL, double bR) {
+        robot.fLeft.setPower(fL);
+        robot.fRight.setPower(fR);
+        robot.bLeft.setPower(bL);
+        robot.bRight.setPower(bR);
+    }
+
+    public void drawSamplingBox(Bitmap bitmap) {
+        double xPercent = (bitmap.getWidth()) / 100.0;
+        double yPercent = (bitmap.getHeight()) / 100.0;
+        Bitmap mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Canvas c = new Canvas(mutableBitmap);
+        Paint p = new Paint();
+        p.setARGB(100, 128, 128, 128);
+        c.drawRect((int) (SAMPLE_LEFT_X_PCT * xPercent), (int) (SAMPLE_TOP_Y_PCT * yPercent), (int) (SAMPLE_RIGHT_X_PCT * xPercent), (int) (SAMPLE_BOT_Y_PCT * yPercent), p);
+        saveBitmap("previewImage.png", mutableBitmap);
+    }
+
+    public boolean isOurJewelonLeft(Bitmap bitmap) {
+        int leftRed = 0;
+        int leftBlue = 0;
+        int count = 0;
+
+        double xPercent = (bitmap.getWidth()) / 100.0;
+        double yPercent = (bitmap.getHeight()) / 100.0;
+
+        telemetry.addData("Start For loop", "");
+        for (int x = SAMPLE_LEFT_X_PCT; x < SAMPLE_RIGHT_X_PCT; x++) { // replace 200 with x pixel size value
+            for (int y = SAMPLE_TOP_Y_PCT; y < SAMPLE_BOT_Y_PCT; y++) {
+                int color = bitmap.getPixel((int) (x * xPercent), (int) (y * yPercent));
+                //telemetry.addData("Color", "%d", color);
+                count++;
+                int red = Color.red(color);
+                int green = Color.green(color);
+                int blue = Color.blue(color);
+
+                double[] HBV = RGBtoHSV(red, green, blue);
+                double hue = HBV[0];
+
+                if (((300 < hue) || (hue < 60)))
+                    leftRed = leftRed + 1;
+
+                if (((180 < hue) && (hue <= 300)))
+                    leftBlue = leftBlue + 1;
+            }
         }
-        public void drawSamplingBox(Bitmap bitmap){
-            double xPercent = (bitmap.getWidth())/100.0;
-            double yPercent = (bitmap.getHeight())/100.0;
-            Bitmap mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
-            Canvas c = new Canvas(mutableBitmap);
-            Paint p = new Paint();
-            p.setARGB(100,128,128,128);
-            c.drawRect((int) (SAMPLE_LEFT_X_PCT*xPercent),(int) (SAMPLE_TOP_Y_PCT*yPercent), (int) (SAMPLE_RIGHT_X_PCT*xPercent),(int) (SAMPLE_BOT_Y_PCT*yPercent), p);
-            saveBitmap("previewImage.png",mutableBitmap);
-        }
+
+
+        String leftJewelColor = "unknown";
+        if (leftRed > leftBlue)
+            leftJewelColor = "red";
+        else
+            leftJewelColor = "blue";
+
+        telemetry.addData("Red count", "%d", leftRed);
+        telemetry.addData("Blue count", "%d", leftBlue);
+        telemetry.addData("Count", "%d", count);
+        telemetry.update();
+
+        if (leftJewelColor.equals(TEAM_COLOR))
+            return true;
+        else
+            return false;
+    }
 }
 
 
