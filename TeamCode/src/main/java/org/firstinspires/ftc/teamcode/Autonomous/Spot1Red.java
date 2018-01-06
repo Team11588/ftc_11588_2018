bb5bc480fb95d49fdcc6d77236285edcb25e3c1a
Gillian N Kearney <kearneyg20428@student.cghsnc.org>
2018-01-06 13:26:37 -0500

    Everything should be ok

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Spot1Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Spot1Red.java
@@ -1,42 +1,6 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.os.Environment;
-
-import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-
-import org.firstinspires.ftc.robotcontroller.internal.LinearOpModeCamera;
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.robotcore.external.navigation.Position;
-
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
-import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
-import org.firstinspires.ftc.teamcode.Hardware.HardwareDxm;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
 
 /**
  * Created by kearneyg20428 on 10/23/2017.
@@ -51,12 +15,8 @@ import java.io.IOException;
  *  4. moves to knock of jewel
  */
 
-
-
-
 @Autonomous(name = "Spot 1 Red")
 public class Spot1Red extends BaseCombinedAutonomous {
-
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -76,11 +36,9 @@ public class Spot1Red extends BaseCombinedAutonomous {
         robot.driveForword(1.25);
 
         turn(90, "right");
-// Ihave discovered that to move in between crypto columbs it is 4/5 of a full rotation
 
-
+        columnMove(mark);
 
         while (opModeIsActive()) ;
     }
-
 }
